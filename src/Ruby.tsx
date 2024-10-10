147ad3c2e585b3e16780d3a255a9aa0c3a77bdd3
Donald33 Wang <donald33wang@gmail.com>
2024-10-10 19:00:44 +0800

    Update Gana

--- src/Ruby.tsx
+++ src/Ruby.tsx
@@ -1,34 +1,34 @@
 import { Fragment, useState } from "react";
 
 export default function Ruby() {
-    const a = `[O]{wû}n[c]{s}[e]{} w[h]{}en I w[a]{ó}[s]{z} si[x]{ks} y[e]{i}[a]{}r[s]{z} [o]{ō}ld I s[a]{ö}[w]{} [a]{ó} m[a]{â}gnifi[c]{s}[e]{ó}nt
-pic[t]{ch}[u]{ó}r[e]{} in [a]{ó} b[o]{u}[o]{}k, c[a]{ö}ll[e]{}d Tr[u]{ü}[e]{} St[o]{ö}ri[e]{}[s]{z} fr[o]{ó}m N[a]{ā}[t]{ch}[u]{ó}r[e]{}, [a]{ó}bout
-[t]{d}h[e]{ó} pr[i]{ī}m[e]{ï}v[a]{ó}l f[o]{ö}r[e]{i}st. It w[a]{ó}[s]{z} [a]{ó} pic[t]{ch}[u]{ó}r[e]{} [o]{ó}[f]{v} [a]{ó} b[o]{ō}[a]{ó} c[o]{ó}nstrict[o]{ó}r in
-[t]{d}h[e]{i} [a]{â}ct [o]{ó}[f]{v} sw[a]{ä}ll[o]{ō}[w]{}ing [a]{ó}n [a]{â}nim[a]{ó}l. H[e]{i}r[e]{} i[s]{z} [a]{ó} copy [o]{ó}[f]{v} [t]{d}h[e]{ó}
-dr[a]{ö}[w]{}ing.`;
+    const a = `[O|wû]nc[e] w[h]en I w[a|á][s|z] si[x|ks] y[e|i][a]r[s|z] [o|ō]ld I saw [a|á] m[a|â]gnific[e|é]nt
+pic[t|ch][u|ú]r[e] in [a|á] b[o|u][o]k, c[a|ö]ll[e]d Tr[u|ü][e] St[o|ö]ri[e][s|z] fr[o|ó]m N[a|ā][t|ch][u|ú]r[e], [a|á]bout
+[t|d]h[e|é] pr[i|ī]m[e|ē]v[a|á]l f[o|ö]r[e|i]st. It w[a|á][s|z] [a|á] pic[t|ch][u|ú]r[e] [o|ó][f|v] [a|á] b[o|ō][a|á] c[o|ó]nstrict[o|ó]r in
+[t|d]h[e|i] [a|â]ct [o|ó][f|v] sw[a|ä]ll[o|ō][w]ing [a|á]n [a|â]nim[a|á]l. H[e|i]r[e] i[s|z] [a|á] copy [o|ó][f|v] [t|d]h[e|é]
+drawing.`;
 
-    const b = `In [t]{d}h[e]{ó} b[o]{u}[o]{}k it s[a]{e}[i]{}d: "B[o]{ō}[a]{ó} c[o]{ó}nstrict[o]{ó}r[s]{z} sw[a]{ä}ll[o]{ō}[w]{} [t]{d}he[i]{}r prey
-[w]{}h[o]{ō}l[e]{}, wi[t]{d}hout ch[e]{ü}[w]{}ing it. [A]{â}ft[e]{ó}r [t]{d}h[a]{â}t [t]{d}hey [a]{ó}r[e]{} not [a]{ā}bl[e]{} t[o]{ó}
-m[o]{ü}v[e]{}, [a]{ó}nd [t]{d}hey sl[e]{ï}[e]{}p thr[o]{}[u]{ü}[gh]{} [t]{d}h[e]{ó} si[x]{ks} m[o]{û}nths [t]{d}h[a]{ó}t [t]{d}hey n[e]{ï}[e]{}d
-f[o]{ó}r d[i]{ī}[g]{j}es[ti]{ch}[o]{ó}n."`;
+    const b = `In [t|d]h[e|é] b[o|u][o]k it s[a|e][i]d: "B[o|ō][a|á] c[o|ó]nstrict[o|ó]r[s|z] sw[a|ä]ll[o|ō][w] [t|d]he[i]r prey
+[w]h[o|ō]l[e], wi[t|d]hout ch[e|ü][w]ing it. [A|â]ft[e|é]r [t|d]h[a|â]t [t|d]hey [a|á]r[e] not [a|ā]bl[e] t[o|ó]
+m[o|ü]v[e], [a|á]nd [t|d]hey sleep thr[o][u|ü][gh] [t|d]h[e|é] si[x|ks] m[o|û]ths [t|d]h[a|á]t [t|d]hey need
+f[o|ó]r d[i|ī][g|j]es[ti|ch][o|ó]n."`;
 
-    const c = `I pond[e]{ó}r[e]{}d d[e]{ï}[e]{}ply, [t]{d}hen, [o]{ō}v[e]{ó}r [t]{d}h[e]{i} [a]{ó}dven[t]{ch}[u]{ó}r[e]{}[s]{z} [o]{ó}[f]{v} [t]{d}h[e]{ó}
-j[u]{û}[n]{ng}gl[e]{}. [A]{â}nd [a]{â}ft[e]{ó}r s[o]{û}m[e]{} w[o]{ë}rk wi[t]{d}h [a]{ó} c[o]{û}l[o]{ó}r[e]{}d pen[c]{s}[i]{ó}l I
-s[u]{ó}c[c]{s}[e]{ï}[e]{}d[e]{i}d in m[a]{ā}king m[y]{ī} f[i]{ë}rst dr[a]{ö}[w]{}ing. M[y]{ī} Dr[a]{ö}[w]{}ing
-N[u]{û}mb[e]{ó}r [O]{wû}n[e]{}. It l[o]{u}[o]{}k[e]{}[d]{t} s[o]{û}m[e]{}thing l[i]{ī}k[e]{} [t]{d}his:`;
+    const c = `I pond[e|é]r[e]d deeply, [t|d]hen, [o|ō]v[e|é]r [t|d]h[e|é] [a|á]dven[t|ch][u|ú]r[e][s|z] [o|ó][f|v] [t|d]h[e|é]
+j[u|û][n|ng]gl[e]. [A|á]nd [a|â]ft[e|é]r s[o|û]m[e] w[o|ë]rk wi[t|d]h [a|á] c[o|û]l[o|ó]r[e]d penc[i|í]l I
+s[u|ú]cceed[e|i]d in m[a|ā]king m[y|ī] f[i|ë]rst drawing. M[y|ī] Drawing
+N[u|û]mb[e|é]r [O|wû]n[e]. It l[o|u][o]k[e][d|t] s[o|û]m[e]thing l[i|ī]k[e] [t|d]his:`;
 
-    const d = `I sh[o]{ō}[we]{}d m[y]{ī} m[a]{â}st[e]{ó}rp[i]{ï}[e]{}[c]{s}[e]{} t[o]{ó} [t]{d}h[e]{ó} gr[o]{ō}[w]{}n-ups, [a]{ó}nd [a]{â}sk[e]{}[d]{t}
-[t]{d}h[e]{ó}m w[h]{}e[t]{d}h[e]{ó}r [t]{d}h[e]{ó} dr[a]{ö}[w]{}ing fr[i]{ī}[gh]{}t[e]{ó}n[e]{}d [t]{d}h[e]{ó}m. B[u]{û}t [t]{d}hey
-[a]{â}ns[w]{}[e]{ó}r[e]{}d: "Fr[i]{ī}[gh]{}t[e]{ó}n? W[h]{}[y]{ī} sh[o]{ó}[ul]{}d [a]{e}ny [o]{wû}n[e]{} b[e]{i} fr[i]{ī}[gh]{}t[e]{ó}n[e]{}d b[y]{ī}
-[a]{ó} h[a]{â}t?"`;
+    const d = `I showed m[y|ī] m[a|â]st[e|é]rp[i|ï][e]c[e] t[o|ó] [t|d]h[e|é] gr[o|ō][w]n-ups, [a|á]nd asked
+[t|d]h[e|é]m w[h]e[t|d]h[e|é]r [t|d]h[e|é] drawing fr[i|ī][gh]t[e|é]n[e]d [t|d]h[e|é]m. B[u|ú]t [t|d]hey
+[a|â]ns[w][e|é]r[e]d: "Fr[i|ī][gh]t[e|é]n? W[h][y|ī] sh[o|ó][ul]d [a|e]ny [o|wû]n[e] b[e|i] fr[i|ī][gh]t[e|é]n[e]d b[y|ī]
+[a|á] h[a|â]t?"`;
 
-    const e = `M[y]{ī} dr[a]{ö}[w]{}ing w[a]{ó}[s]{z} not [a]{ó} pic[t]{ch}[u]{ó}r[e]{} [o]{ó}[f]{v} [a]{ó} h[a]{â}t. It w[a]{ó}[s]{z} [a]{ó} pic[t]{ch}[u]{ó}r[e]{}
-[o]{ó}[f]{v} [a]{ó} b[o]{ō}[a]{ó} c[o]{ó}nstrict[o]{ó}r d[i]{ī}[g]{j}esting [a]{ó}n el[e]{i}[p]{f}[h]{}[a]{ó}nt. B[u]{û}t sin[c]{s}[e]{} [t]{d}h[e]{ó}
-gr[o]{ō}[w]{}n-ups w[e]{ó}r[e]{} not [a]{ā}bl[e]{} t[o]{u} [u]{û}nd[e]{ó}rst[a]{â}nd it, I m[a]{ā}d[e]{} [a]{ó}n[o]{û}[t]{d}h[e]{ó}r
-dr[a]{ö}[w]{}ing: I dr[e]{ü}[w]{} [t]{d}h[e]{i} ins[i]{ī}d[e]{} [o]{ó}[f]{v} [a]{ó} b[o]{ō}[a]{ó} c[o]{ó}nstrict[o]{ó}r, s[o]{ō} [t]{d}h[a]{ó}t [t]{d}h[e]{ó}
-gr[o]{ō}[w]{}n-ups c[o]{ó}[ul]{}d s[e]{ï}[e]{} it cl[e]{i}[a]{}rly. [T]{d}hey [a]{ö}lw[a]{ā}[y]{}[s]{z} n[e]{ï}[e]{}d t[o]{ó} h[a]{ó}v[e]{}
-things [e]{i}[x]{ks}pl[a]{ā}[i]{}n[e]{}d. M[y]{ī} Dr[a]{ö}[w]{}ing N[u]{û}mb[e]{ó}r T[w]{}[o]{ü} l[o]{u}[o]{}k[e]{}[d]{t} l[i]{ī}k[e]{}
-[t]{d}his:`;
+    const e = `M[y|ī] drawing w[a|á][s|z] not [a|á] pic[t|ch][u|ú]r[e] [o|ó][f|v] [a|á] h[a|â]t. It w[a|á][s|z] [a|á] pic[t|ch][u|ú]r[e]
+[o|ó][f|v] [a|á] b[o|ō][a|á] c[o|ó]nstrict[o|ó]r d[i|ī][g|j]esting [a|á]n el[e|i][p|f][h][a|á]nt. B[u|ú]t sinc[e] [t|d]h[e|é]
+gr[o|ō][w]n-ups w[e|é]r[e] not [a|ā]bl[e] t[o|ó] [u|û]nd[e|é]rst[a|â]nd it, I m[a|ā]d[e] [a|á]n[o|û][t|d]h[e|é]r
+drawing: I dr[e|ü][w] [t|d]h[e|é] ins[i|ī]d[e] [o|ó][f|v] [a|á] b[o|ō][a|á] c[o|ó]nstrict[o|ó]r, s[o|ō] [t|d]h[a|á]t [t|d]h[e|é]
+gr[o|ō][w]n-ups c[o|ó][ul]d see it cl[e|i][a]rly. [T|d]hey [a|ö]lway[s|z] need t[o|ó] h[a|á]v[e]
+things [e|i][x|ks]pl[a|ā][i]n[e]d. M[y|ī] Drawing N[u|û]mb[e|é]r T[w][o|ü] l[o|u][o]k[e][d|t] l[i|ī]k[e]
+[t|d]his:`;
 
     const [hide, toggleHide] = useState(false);
 
@@ -36,9 +36,9 @@ things [e]{i}[x]{ks}pl[a]{ā}[i]{}n[e]{}d. M[y]{ī} Dr[a]{ö}[w]{}ing N[u]{û
         <main className="english-gana">
             <h4>字母表</h4>
             <Alphabet />
-            {/* <button type="button" onClick={() => toggleHide(!hide)}>
+            <button type="button" onClick={() => toggleHide(!hide)}>
                 ToggleHide
-            </button> */}
+            </button>
             <summary>
                 <div>字母上面有一横表示字母表里的读音，比如 ā ē ī ō ū 分别对应 A E I O U</div>
                 <div>字母上面有两点表示长音，比如 ä ï ö ü ë 分别对应 car ski dog flu nerd（ë 比较特别）</div>
@@ -47,6 +47,7 @@ things [e]{i}[x]{ks}pl[a]{ā}[i]{}n[e]{}d. M[y]{ī} Dr[a]{ö}[w]{}ing N[u]{û
                 </div>
                 <div>字母上面有三角用来表示 â û，分别对应 cat up</div>
                 <div>下划线表示这些字母不发音</div>
+                <div>其余的是常见的字母组合发音</div>
             </summary>
             <Ipa />
             <EnglishGana s={a} hide={hide} />
@@ -54,7 +55,7 @@ things [e]{i}[x]{ks}pl[a]{ā}[i]{}n[e]{}d. M[y]{ī} Dr[a]{ö}[w]{}ing N[u]{û
             <EnglishGana s={b} />
             <br />
             <EnglishGana s={c} />
-            <br />o
+            <br />
             <EnglishGana s={d} />
             <br />
             <EnglishGana s={e} />
@@ -64,32 +65,32 @@ things [e]{i}[x]{ks}pl[a]{ā}[i]{}n[e]{}d. M[y]{ī} Dr[a]{ö}[w]{}ing N[u]{û
 
 export function Alphabet() {
     const list = [
-        ["a", "[a]{â}ppl[e]{}"],
+        ["a", "[a|â]ppl[e]"],
         ["b", "boy"],
-        ["c", "c[a]{â}t"],
-        ["d", "d[o]{ö}g"],
-        ["e", "egg"],
+        ["c", "coat"],
+        ["d", "d[o|ö]g"],
+        ["e", "essay"],
         ["f", "fish"],
-        ["g", "g[i]{ë}rl"],
-        ["h", "hous[e]{}"],
+        ["g", "g[a|â][t|d]h[e|é]r"],
+        ["h", "how"],
         ["i", "input"],
-        ["j", "join"],
-        ["k", "k[i]{ī}t[e]{}"],
-        ["l", "l[u]{û}nch"],
-        ["m", "m[u]{yü}[s]{z}ic"],
-        ["n", "n[e]{ï}[e]{}d"],
-        ["o", "o[x]{ks}"],
-        ["p", "p[e]{ë}rs[o]{ó}n"],
+        ["j", "jaw"],
+        ["k", "k[i|ī]t[e]"],
+        ["l", "l[u|û]nch"],
+        ["m", "m[u|ū][s|z]ic"],
+        ["n", "need"],
+        ["o", "o[x|ks]"],
+        ["p", "p[e|ë]rs[o|ó]n"],
         ["q", "quick"],
-        ["r", "r[u]{ü}by"],
-        ["s", "sk[i]{ï}"],
-        ["t", "t[o]{ü}[o]{}th"],
-        ["u", "[u]{û}pd[a]{ā}t[e]{}"],
-        ["v", "vow[e]{ó}l"],
-        ["w", "we[a]{}[t]{d}h[e]{ó}r"],
+        ["r", "r[u|ü]by"],
+        ["s", "sk[i|ï]"],
+        ["t", "tooth"],
+        ["u", "[u|û]pd[a|ā]t[e]"],
+        ["v", "v[i]ew"],
+        ["w", "weak"],
         ["x", "xenon"],
-        ["y", "y[a]{ä}rd"],
-        ["z", "z[e]{i}r[o]{ō}"],
+        ["y", "y[a|ä]rd"],
+        ["z", "z[e|i]r[o|ō]"],
     ];
 
     return (
@@ -110,37 +111,37 @@ export function Ipa() {
     const ipaList = [
         ["a", "(a)", "", "", ""],
         ["ɪ", "i", "hit", "bit", "kid"],
-        ["ʊ", "u", "put", "bush", "[s]{sh}ug[a]{ó}r"],
+        ["ʊ", "u", "put", "bush", "[s|sh]ug[a|á]r"],
         ["e", "e", "egg", "pet", "jet"],
-        ["ɒ", "o", "not", "top", "hot"],
+        ["ɒ", "(o)", "not", "top", "hot"],
 
-        ["ɑː", "ä", "c[a]{ä}r", "f[a]{ä}r", "w[a]{ä}[t]{}ch"],
-        ["iː", "ï", "sk[i]{ï}", "p[i]{ï}[e]{}[c]{s}[e]{}", "f[i]{ï}[e]{}ld"],
-        ["uː", "ü", "r[u]{ü}by", "fr[u]{ü}[i]{}t", "s[u]{ü}p[e]{ó}r"],
-        ["ɔː", "ö", "d[o]{ö}g", "f[o]{ö}rk", "m[o]{ö}r[e]{}"],
+        ["ɑː", "ä", "c[a|ä]r", "f[a|ä][t|d]h[e|é]r", "w[a|ä]tch"],
+        ["iː", "ï", "sk[i|ï]", "p[i|ï][e]c[e]", "f[i|ï][e]ld"],
+        ["uː", "ü", "r[u|ü]by", "r[u|ü]l[e]", "s[u|ü]p[e|é]r"],
+        ["ɔː", "ö", "d[o|ö]g", "b[o|ö]ss", "m[o|ö]r[e]"],
 
-        ["ɜː", "ë", "p[e]{ë}rs[o]{ó}n", "p[e]{ë}rf[e]{i}ct", "n[e]{ë}rd"],
+        ["ɜː", "ë", "p[e|ë]rs[o|ó]n", "p[e|ë]rf[e|i]ct", "n[e|ë]rd"],
 
-        ["æ", "â", "c[a]{â}t", "f[a]{â}t", "h[a]{â}t"],
-        ["ʌ", "û", "[u]{û}p", "l[u]{û}ck", "sh[u]{û}t"],
+        ["æ", "â", "c[a|â]t", "f[a|â]t", "h[a|â]t"],
+        ["ʌ", "û", "[u|û]p", "l[u|û]ck", "sh[u|û]t"],
 
-        ["eɪ", "ā", "t[a]{ā}k[e]{}", "m[a]{ā}k[e]{}", "l[a]{ā}k[e]{}"],
-        ["iː", "ē", "n[e]{ï}[e]{}d", "f[e]{ï}[e]{}d", "m[e]{ï}[e]{}t"],
-        ["aɪ", "ī", "k[i]{ī}t[e]{}", "l[i]{ī}k[e]{}", "b[i]{ī}k[e]{}"],
-        ["əʊ", "ō", "n[o]{ō}", "r[o]{ō}p[e]{}", "h[o]{ō}ld"],
-        ["juː", "ū", "m[u]{yü}[s]{z}ic", "[u]{yü}[s]{z}[e]{}", "c[u]{yü}[e]{}"],
+        ["eɪ", "ā", "t[a|ā]k[e]", "m[a|ā]k[e]", "l[a|ā]k[e]"],
+        ["iː", "ē", "k[e|ē][y]", "p[e|ē][o]pl[e]", "s[e|ē][i]z[e]"],
+        ["aɪ", "ī", "k[i|ī]t[e]", "l[i|ī]k[e]", "b[i|ī]k[e]"],
+        ["əʊ", "ō", "n[o|ō]", "r[o|ō]p[e]", "h[o|ō]ld"],
+        ["juː", "ū", "m[u|ū][s|z]ic", "[u|ū][s|z][e]", "c[u|ū][e]"],
 
-        ["ɔɪ", "oi", "join", "voi[c]{s}[e]{}", "boy"],
-        ["aʊ", "(au)", "out", "hous[e]{}", "now"],
+        ["ɔɪ", "oi", "join", "voic[e]", "boy"],
+        ["aʊ", "(au)", "out", "hous[e]", "now"],
 
-        ["tʃ", "ch", "l[u]{û}nch", "check", "chip"],
-        ["dʒ", "j", "jet", "jog", "j[o]{ō}ke"],
-        ["θ", "th", "t[o]{ü}[o]{}th", "th[e]{ï}m[e]{}", "w[a]{ö}rmth"],
-        ["ð", "dh", "[t]{d}hey", "[t]{d}his", "we[a]{}[t]{d}h[e]{ó}r"],
+        ["tʃ", "ch", "l[u|û]nch", "check", "chip"],
+        ["dʒ", "j", "jet", "jog", "j[o|ō]ke"],
+        ["θ", "th", "tooth", "th[e|ï]m[e]", "w[a|ö]rmth"],
+        ["ð", "dh", "[t|d]hey", "[t|d]his", "we[a][t|d]h[e|é]r"],
         ["ʃ", "sh", "fish", "ship", "wish"],
-        ["ʒ", "zh", "vi[si]{zh}[o]{ó}n", "me[a]{}[s]{zh}[u]{ó}r[e]{}", "ple[a]{}[s]{zh}[u]{ó}r[e]{}"],
-        ["j", "y", "yes", "yell", "y[a]{ä}rd"],
-        ["ŋ", "ng", "thing", "thi[n]{ng}k", "fi[n]{ng}ger"],
+        ["ʒ", "zh", "vi[si|zh][o|ó]n", "me[a][s|zh][u|ú]r[e]", "ple[a][s|zh][u|ú]r[e]"],
+        ["j", "y", "yes", "yell", "y[a|ä]rd"],
+        ["ŋ", "ng", "thing", "think", "fi[n|ng]ger"],
     ];
 
     return (
@@ -164,7 +165,7 @@ export function Ipa() {
 
 export function EnglishGana({ s, hide = false }: { s: string; hide?: boolean }) {
     const result = [];
-    let rb = [];
+    let rb = []; // also used for plain text
     let rt = [];
     let isRt = false;
     for (const ch of s) {
@@ -175,12 +176,10 @@ export function EnglishGana({ s, hide = false }: { s: string; hide?: boolean }) 
                     rb = [];
                 }
                 break;
-            case "]":
-                break;
-            case "{":
+            case "|":
                 isRt = true;
                 break;
-            case "}":
+            case "]":
                 result.push(toRuby(rb.join(""), rt.join(""), hide));
                 rb = [];
                 rt = [];
@@ -200,6 +199,8 @@ export function EnglishGana({ s, hide = false }: { s: string; hide?: boolean }) 
         rb = [];
     }
 
+    console.log(result);
+
     return (
         <div className="ruby-wrap">
             {result.map((value, index) => {
@@ -218,44 +219,28 @@ export function toRuby(rb: string, rt: string, hide: boolean): any {
         return <u>{rb}</u>;
     }
 
-    if (rt === "ï" && rb.toLowerCase() === "e") {
-        const ch = "ē";
-        return rb === "e" ? ch : ch.toUpperCase();
-    }
-    if (rt === "yü" && rb.toLowerCase() === "u") {
-        const ch = "ū";
-        return rb === "u" ? ch : ch.toUpperCase();
-    }
-
-    const upper = { A: 0, E: 0, I: 0, O: 0, U: 0 };
-    const schwa: any = {
-        a: "á",
-        e: "é",
-        i: "í",
-        o: "ó",
-        u: "ú",
-    };
-    if (rt === "ó" && rb.toLowerCase() in schwa) {
-        if (rb in upper) {
-            return schwa[rb.toLowerCase()].toUpperCase();
-        }
-        return schwa[rb];
-    }
-
+    const upper = ["A", "E", "I", "O", "U"];
     const diacritic: any = {
         â: "a",
         ā: "a",
         ä: "a",
+        ē: "e",
         ë: "e",
         ī: "i",
         ï: "i",
         ō: "o",
         ö: "o",
         û: "u",
+        ū: "u",
         ü: "u",
+        á: "a",
+        é: "e",
+        í: "i",
+        ó: "o",
+        ú: "u",
     };
     if (rt in diacritic && diacritic[rt] === rb.toLowerCase()) {
-        if (rb in upper) {
+        if (upper.includes(rb)) {
             return rt.toUpperCase();
         }
         return rt;
